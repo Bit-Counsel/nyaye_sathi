840d73e1fe2491d117a82a66514c34ade816d3c2
Tabish <mohdtabishkhan001@gmail.com>
2023-10-25 19:18:07 +0530

    tested routing on react-router-dom version 6

--- src/App.js
+++ src/App.js
@@ -3,32 +3,18 @@ import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// import 
+import { BrowserRouter , Route, Routes } from "react-router-dom";
+// import
 
 function App() {
   return (
-    <>
-    <Router>
-      <Switch>
-      {/* <Route exact path = "/Card">
-        <Card>
-      </Route> */}
-      <Route exact path="/SignIn">
-        <SignIn />
-        </Route>
-
-        <Route exact path="/SignUp">
-        <SignUp />
-        </Route>
-
-        <Route exact path ="/">
-        <LandingPage/>
-        </Route>
-
-      </Switch>
-      </Router>
-    </>
+    
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<LandingPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    
   );
 }
 
